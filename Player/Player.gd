543b6691d67cbfb5478d03379a6122ab321c5c30
dimitriksky <70145560+dimitriksky@users.noreply.github.com>
2024-04-16 13:56:20 +0300

    Edit movement to add acceleration and friction, change project settings

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,13 +1,20 @@
 extends CharacterBody2D
 
-const MOVE_SPEED: float = 100
+const ACCELERATION = 2
+const FRICTION = 3
+const MAX_SPEED = 100
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var input_direction = Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"), 
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 		).normalized()
-		
-	velocity = input_direction * MOVE_SPEED
+	
+	if input_direction != Vector2.ZERO:
+		velocity += input_direction * ACCELERATION
+		velocity = velocity.limit_length(MAX_SPEED)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
+	
 	move_and_slide()
 	
