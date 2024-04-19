5f1e4bef6ba5e7f3dc73d97d02d8899aea5f00f3
dimitriksky <70145560+dimitriksky@users.noreply.github.com>
2024-04-19 16:17:21 +0300

    Complete player's movement and tweak speed and friction

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 
-const ACCELERATION = 6
-const FRICTION = 10
-const MAX_SPEED = 100
+const ACCELERATION = 50
+const FRICTION = 50
+const MAX_SPEED = 80
 
-@onready var animationPlayer = $AnimationPlayer
+@onready var animation_player = $AnimationPlayer
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
 
 func _physics_process(_delta):
 	var input_direction = Vector2(
@@ -14,10 +16,12 @@ func _physics_process(_delta):
 	
 	if input_direction != Vector2.ZERO:
 		velocity = velocity.move_toward(input_direction * MAX_SPEED, ACCELERATION)
-		animationPlayer.play("run_right")
+		animation_tree.set("parameters/Run/blend_position", input_direction)
+		animation_tree.set("parameters/Idle/blend_position", input_direction)
+		state_machine.travel("Run")
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
-		animationPlayer.play("idle_right")
-	
+		state_machine.travel("Idle")
+		
 	move_and_slide()
 	
